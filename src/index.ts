fcc607548c2219baf4490c9deece0a4398ea208a
Andrey Allyson <andreyuser47@gmail.com>
2023-06-30 18:06:55 -0300

    renamed named export from 'XLSXHandlers' to 'excelHandlers'

--- src/index.ts
+++ src/index.ts
@@ -2,6 +2,6 @@ export * from './auto-schedule';
 
 export * as lib from './extra-duty-lib';
 
-export * as XLSXHandlers from './xlsx-handlers';
+export * as excelHandles from './xlsx-handlers';
 
 export * as utils from './utils';
