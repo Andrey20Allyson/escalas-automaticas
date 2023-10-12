bb09161119c629af0fe44c0a384eedfe8f82a6d5
Andrey Allyson <andreyuser47@gmail.com>
2023-10-12 15:03:11 -0300

    test: added a test to #colliesWithTomorrowWork method

--- src/extra-duty-lib/structs/extra-duty.spec.ts
+++ src/extra-duty-lib/structs/extra-duty.spec.ts
@@ -11,7 +11,8 @@ interface DutyMockConfig {
 }
 
 interface WorkerMockConfig {
-  isDailyWorker: boolean;
+  daysOfWork: 'daily-worker' | number[];
+  workTime?: WorkTime;
 }
 
 interface MocksConfig {
@@ -34,9 +35,9 @@ function mocks(config: MocksConfig) {
     year: mockedYear,
   });
 
-  const daysOfWork = config.worker.isDailyWorker
+  const daysOfWork = config.worker.daysOfWork === 'daily-worker'
     ? DaysOfWork.fromDailyWorker(mockedYear, mockedMonth)
-    : DaysOfWork.fromDays([0, 1, 4, 5, 7, 8, 20, 21, 22, 26, 27], mockedYear, mockedMonth);
+    : DaysOfWork.fromDays(config.worker.daysOfWork, mockedYear, mockedMonth);
 
   const worker = new WorkerInfo({
     daysOfWork,
@@ -47,58 +48,77 @@ function mocks(config: MocksConfig) {
     post: 'N/A',
     postWorkerID: 0,
     workerID: 0,
-    workTime: new WorkTime(7, 12),
+    workTime: config.worker.workTime ?? new WorkTime(7, 12),
   });
 
   return { duty, worker };
 }
 
 function createIsDailyWorkerAtFridayAtNightTest(nameSufix: string, mockConfig: MocksConfig, expectedReturn: boolean) {
-  test(`#${ExtraDuty.prototype.isDailyWorkerAtFridayAtNight.name} shold return ${expectedReturn} if ${nameSufix}`, () => {
+  test(`shold return ${expectedReturn} if ${nameSufix}`, () => {
     const { duty, worker } = mocks(mockConfig);
 
     expect(duty.isDailyWorkerAtFridayAtNight(worker)).toEqual(expectedReturn);
   });
 }
 
-describe(WorkTime.name, () => {
-  createIsDailyWorkerAtFridayAtNightTest(`extra duty is at friday night and worker is a daily worker`, {
-    duty: {
-      day: 5,
-      index: 1,
-    },
-    worker: {
-      isDailyWorker: true
-    }
-  }, true);
+describe(ExtraDuty.name, () => {
+  describe(ExtraDuty.prototype.isDailyWorkerAtFridayAtNight.name, () => {
+    createIsDailyWorkerAtFridayAtNightTest(`extra duty is at friday night and worker is a daily worker`, {
+      duty: {
+        day: 5,
+        index: 1,
+      },
+      worker: {
+        daysOfWork: 'daily-worker'
+      }
+    }, true);
 
-  createIsDailyWorkerAtFridayAtNightTest(`extra duty isn't at friday`, {
-    duty: {
-      day: 4,
-      index: 1,
-    },
-    worker: {
-      isDailyWorker: true
-    }
-  }, false);
+    createIsDailyWorkerAtFridayAtNightTest(`extra duty isn't at friday`, {
+      duty: {
+        day: 4,
+        index: 1,
+      },
+      worker: {
+        daysOfWork: 'daily-worker'
+      }
+    }, false);
 
-  createIsDailyWorkerAtFridayAtNightTest(`extra duty is at friday but isn't at night`, {
-    duty: {
-      day: 5,
-      index: 0,
-    },
-    worker: {
-      isDailyWorker: true
-    }
-  }, false);
+    createIsDailyWorkerAtFridayAtNightTest(`extra duty is at friday but isn't at night`, {
+      duty: {
+        day: 5,
+        index: 0,
+      },
+      worker: {
+        daysOfWork: 'daily-worker'
+      }
+    }, false);
 
-  createIsDailyWorkerAtFridayAtNightTest(`extra duty is at friday and is at night bot worker isn't a daily worker`, {
-    duty: {
-      day: 5,
-      index: 0,
-    },
-    worker: {
-      isDailyWorker: false,
-    }
-  }, false);
-})
+    createIsDailyWorkerAtFridayAtNightTest(`extra duty is at friday and is at night bot worker isn't a daily worker`, {
+      duty: {
+        day: 5,
+        index: 0,
+      },
+      worker: {
+        daysOfWork: [],
+      }
+    }, false);
+  });
+
+  describe(ExtraDuty.prototype.collidesWithTomorrowWork.name, () => {
+    test(`shold return true if duty collides with tomorrow work time off`, () => {
+      const { duty, worker } = mocks({
+        duty: {
+          day: 7,
+          index: 1,
+        },
+        worker: {
+          daysOfWork: [8],
+          workTime: new WorkTime(7, 12),
+        },
+      });
+
+      expect(duty.collidesWithTomorrowWork(worker)).toBeTruthy();
+    });
+  });
+});
