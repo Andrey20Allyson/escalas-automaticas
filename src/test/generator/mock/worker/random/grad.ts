e29759fabb2ae778f47a9805e7ce1cb350394876
Andrey Allyson <andreyuser47@gmail.com>
2023-10-15 18:10:13 -0300

    refactor: changed gcm mock incidence

--- src/test/generator/mock/worker/random/grad.ts
+++ src/test/generator/mock/worker/random/grad.ts
@@ -1,7 +1,7 @@
 export function randomGrad(): string {
   const choice = Math.random();
 
-  if (choice < .6) return 'GCM';
+  if (choice < .5) return 'GCM';
   if (choice < .8) return 'SI';
 
   return 'INSP';
