4f98b0146b1aa293f748518f3a95258f7dddf6de
Andrey Allyson <andreyuser47@gmail.com>
2023-10-15 18:09:31 -0300

    feat: implemented 'table integrity' checking to generator test

--- src/test/generator/index.ts
+++ src/test/generator/index.ts
@@ -1,9 +1,11 @@
 import { ExtraDutyTableV2 } from '../../extra-duty-lib';
 import { WorkerMocker } from './mock/worker';
 import { Benchmarker, analyseResult } from '../../utils';
+import { TableIntegrity, TableIntegrityAnalyser } from '../../extra-duty-lib/extra-duty-table/integrity';
 
 const workerMocker = new WorkerMocker();
 const beckmarker = new Benchmarker();
+const integrityAnalyser = TableIntegrityAnalyser.fromDefault();
 
 const year = 2023;
 const month = 10;
@@ -13,11 +15,17 @@ const table = new ExtraDutyTableV2({ month, year });
 
 const tableAssignBenchmark = beckmarker.start('talbe assign');
 
-table.tryAssignArrayMultipleTimes(workers, 64);
+table.tryAssignArrayMultipleTimes(workers, 1);
 
 tableAssignBenchmark.end();
 
 table.calculatePontuation(table.firstMonday);
+
+const tableAnalisysBenchmark = beckmarker.start('table analisys');
+
+const integrity = integrityAnalyser.analyse(table, new TableIntegrity(40000));
+
+tableAnalisysBenchmark.end();
 
 const analisysString = analyseResult(table);
 console.log(analisysString);
@@ -25,5 +33,5 @@ console.log(analisysString);
 const benchmarkString = beckmarker.getMessage();
 console.log(benchmarkString);
 
-const pontuation = table.getPontuation();
-console.log(`pontuation: ${pontuation}`);
+console.log(integrity);
+console.log(`está integro: ${integrity.isCompliant()}`);
