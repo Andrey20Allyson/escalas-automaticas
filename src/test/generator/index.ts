c001323cf4b3dfeec9475b18db5bc8e10cc73191
Andrey Allyson <andreyuser47@gmail.com>
2023-10-18 02:41:14 -0300

    test: added cli inputs for test

--- src/test/generator/index.ts
+++ src/test/generator/index.ts
@@ -1,28 +1,75 @@
-import { ExtraDutyTableV2 } from '../../extra-duty-lib';
-import { DefaultTableIntegrityAnalyser, TableIntegrity } from '../../extra-duty-lib/extra-duty-table/integrity';
-import { Benchmarker, analyseResult } from '../../utils';
+import fs from 'fs/promises';
+import { parseWorkers } from '../../auto-schedule/io';
+import { ExtraDutyTableV2, Holidays, WorkerRegistriesMap } from '../../extra-duty-lib';
+import { Benchmarker, Result, analyseResult } from '../../utils';
+import { argvCompiler } from '../../utils/cli';
 import { WorkerMocker } from './mock/worker';
 
-const workerMocker = new WorkerMocker();
-const beckmarker = new Benchmarker();
+function mockWorkers(year: number, month: number) {
+  const workerMocker = new WorkerMocker();
 
-const year = 2023;
-const month = 10;
+  return workerMocker.createArray(28, { mode: 'random', config: { month, year } });
+}
 
-const workers = workerMocker.createArray(28, { mode: 'random', config: { month, year } });
-const table = new ExtraDutyTableV2({ month, year });
+async function loadWorkers(year: number, month: number, inputFile: string) {
+  const inputBuffer = await fs.readFile(inputFile);
+  const registriesFileBuffer = await fs.readFile('input/registries.json');
+  const holidaysFileBuffer = await fs.readFile('./input/feriados.json');
 
-const tableAssignBenchmark = beckmarker.start('talbe assign');
+  const workerRegistryMap = Result.unwrap(WorkerRegistriesMap.parseJSON(registriesFileBuffer));
 
-table.tryAssignArrayMultipleTimes(workers, 400);
+  const holidays = Result.unwrap(Holidays.safeParse(holidaysFileBuffer));
 
-tableAssignBenchmark.end();
+  return parseWorkers(inputBuffer, {
+    workerRegistryMap,
+    holidays,
+    month,
+    year,
+  });
+}
 
-const analisysString = analyseResult(table);
-console.log(analisysString);
+export type WorkersLoadMode = 'mock' | 'input-file';
 
-const benchmarkString = beckmarker.getMessage();
-console.log(benchmarkString);
+export interface TestExecOptions {
+  mode?: WorkersLoadMode;
+  inputFile?: string;
+}
 
-console.log(table.integrity);
-console.log(`pode ser utilizado: ${table.integrity.isCompliant()}`);
+async function exec(options: TestExecOptions = {}) {  
+  const {
+    mode = options.inputFile !== undefined ? 'input-file' : 'mock',
+    inputFile = 'input/data.xlsx',
+  } = options;
+  
+  const beckmarker = new Benchmarker();
+  const year = 2023;
+  const month = 10;
+
+  const workers = mode === 'mock'
+    ? mockWorkers(year, month)
+    : await loadWorkers(year, month, inputFile);
+
+  const table = new ExtraDutyTableV2({ month, year });
+
+  const tableAssignBenchmark = beckmarker.start('talbe assign');
+
+  table.tryAssignArrayMultipleTimes(workers, 400);
+
+  tableAssignBenchmark.end();
+
+  const analisysString = analyseResult(table);
+  console.log(analisysString);
+
+  const benchmarkString = beckmarker.getMessage();
+  console.log(benchmarkString);
+
+  console.log(table.integrity);
+  console.log(`pode ser utilizado: ${table.integrity.isCompliant()}`);
+}
+
+const cliController = argvCompiler.compile();
+
+exec({
+  mode: cliController.optionalFlag('mode', 'M')?.asEnum(['mock', 'input-file']),
+  inputFile: cliController.optionalFlag('input', 'I')?.asString(),
+});
