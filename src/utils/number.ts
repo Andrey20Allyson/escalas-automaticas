98e72d80bdb6227d718c00296e31699a4e616b16
Andrey Allyson <andreyuser47@gmail.com>
2023-10-20 00:06:12 -0300

    feat: added #isInteger function to utils

--- src/utils/number.ts
+++ src/utils/number.ts
@@ -3,3 +3,7 @@ export function parseNumberOrThrow(value?: unknown) {
   if (isNaN(num)) throw new Error(`Can't parse "${value}" because results in NaN!`);
   return num;
 }
+
+export function isInteger(value: number) {
+  return value % 1 === 0;
+}
