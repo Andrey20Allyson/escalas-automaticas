29648c331163789c70b9dd5134a51a7027a4a033
Andrey Allyson <andreyuser47@gmail.com>
2024-01-17 17:58:40 -0300

    feat: added new rules to jb assigner

--- src/extra-duty-lib/builders/assigners/jb-assigner.ts
+++ src/extra-duty-lib/builders/assigners/jb-assigner.ts
@@ -1,12 +1,13 @@
 import { ScheduleAssigner } from "./assigner";
 import { AssignmentChecker } from "./checking";
+import { JBNightAssignmentRule } from "./checking/rules/jb-night-rule";
 import { JQScheduleAssigner } from "./jq-assigner";
 
 export class JBScheduleAssigner extends ScheduleAssigner {
   constructor() {
     const jqAssignmentChecker = new JQScheduleAssigner().assignmentChecker;
     const jbAssignmentChecker = new AssignmentChecker([
-      
+      new JBNightAssignmentRule(),
     ]).extend(jqAssignmentChecker);
 
     super(jbAssignmentChecker);
