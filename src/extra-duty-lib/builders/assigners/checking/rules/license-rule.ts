4b620545b4b5514b409404e1098caf1486715500
Andrey Allyson <andreyuser47@gmail.com>
2024-01-17 14:55:07 -0300

    style: formatted

--- src/extra-duty-lib/builders/assigners/checking/rules/license-rule.ts
+++ src/extra-duty-lib/builders/assigners/checking/rules/license-rule.ts
@@ -3,7 +3,7 @@ import { AssignmentRule } from "../assignment-rule";
 
 export class LicenseAssignmentRule implements AssignmentRule {
   canAssign(table: ExtraDutyTable, worker: WorkerInfo, dayIndex: number): boolean {
-    return this.canAssignInDay(table, worker,dayIndex);
+    return this.canAssignInDay(table, worker, dayIndex);
   }
 
   canAssignInDay(_table: ExtraDutyTable, worker: WorkerInfo, dayIndex: number): boolean {
