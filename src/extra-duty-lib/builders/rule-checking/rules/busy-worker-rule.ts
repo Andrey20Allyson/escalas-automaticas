53f6c88897cd60ae6c56df2a6855c178dbea8e9e
Andrey Allyson <andreyuser47@gmail.com>
2024-01-30 17:44:25 -0300

    feat: now BusyWorkerAssigmentRule can recive the table as second param

--- src/extra-duty-lib/builders/rule-checking/rules/busy-worker-rule.ts
+++ src/extra-duty-lib/builders/rule-checking/rules/busy-worker-rule.ts
@@ -1,8 +1,10 @@
-import { ExtraDuty, ExtraDutyTableConfig, WorkerInfo } from "../../../structs";
+import { ExtraDuty, ExtraDutyTable, WorkerInfo } from "../../../structs";
 import { AssignmentRule } from "../assignment-rule";
 
 export class BusyWorkerAssignmentRule implements AssignmentRule {
-  canAssign(worker: WorkerInfo, duty: ExtraDuty): boolean {
-    return duty.table.limiter.isLimitOut(worker) === false;
+  canAssign(worker: WorkerInfo, arg1: ExtraDuty | ExtraDutyTable): boolean {
+    const table = arg1 instanceof ExtraDutyTable ? arg1 : arg1.table;
+    
+    return table.limiter.isLimitOut(worker) === false;
   }
 }
