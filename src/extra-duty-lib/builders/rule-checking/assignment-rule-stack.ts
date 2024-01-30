c003e5bc8c913ba1e317dbec914c727675c66589
Andrey Allyson <andreyuser47@gmail.com>
2024-01-30 17:42:33 -0300

    feat: added #find for AssignmentRuleStack for rule searching

--- src/extra-duty-lib/builders/rule-checking/assignment-rule-stack.ts
+++ src/extra-duty-lib/builders/rule-checking/assignment-rule-stack.ts
@@ -3,7 +3,7 @@ import { AssignmentRule } from "./assignment-rule";
 
 export class AssignmentRuleStack implements AssignmentRule {
   readonly rules: ReadonlyArray<AssignmentRule>;
-  
+
   constructor(
     rules: AssignmentRule[] = [],
   ) {
@@ -18,5 +18,31 @@ export class AssignmentRuleStack implements AssignmentRule {
 
   use(...rules: AssignmentRule[]): AssignmentRuleStack {
     return new AssignmentRuleStack(this.rules.concat(rules));
+  }
+
+  private _find(predicate: (rule: AssignmentRule) => boolean, root: AssignmentRule = this): AssignmentRule | null {
+    if (predicate(root)) return root;
+    
+    if (root instanceof AssignmentRuleStack) {
+      for (const rule of root.rules) {
+        const found = this._find(predicate, rule);
+        
+        if (found !== null) return found;
+      }
+    }
+
+    return null;
+  }
+
+  find<T extends AssignmentRule>(predicate: (rule: AssignmentRule) => rule is T): T | null;
+  find(predicate: (rule: AssignmentRule) => boolean): AssignmentRule | null;
+  find(predicate: (rule: AssignmentRule) => boolean): AssignmentRule | null {
+    return this._find(predicate);
+  }
+
+  static find<T extends AssignmentRule>(rule: AssignmentRule, predicate: (rule: AssignmentRule) => rule is T): T | null;
+  static find(rule: AssignmentRule, predicate: (rule: AssignmentRule) => boolean): AssignmentRule | null;
+  static find(rule: AssignmentRule, predicate: (rule: AssignmentRule) => boolean) {
+    return new AssignmentRuleStack([rule]).find(predicate);
   }
 }
